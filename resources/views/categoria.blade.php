0f67c95cde790b0e64f896acd2afaf1977f0ae24
GianniErrera <giannierrera@yahoo.it>
2018-09-11 18:12:20 +0200

    Aggiunte opzioni modifica/rimuovi/aggiungi per admin nella vista per categoria

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -11,18 +11,14 @@
      @endforeach
 
 
-{{--       @if (Auth::user()->isAdmin == 1)
+      @if (Auth::user()->isAdmin == 1)
         <div class = "form-group">
   <form method = "GET" action = "/prodotti/inserisci" > 
      {{csrf_field()}}
     <button type="submit" class="btn btn-primary">Nuovo prodotto</button></form>
 </div>
-@endif --}}
+@endif
 
-       
-
-
-
- 
+   
 
 @endsection
--- resources/views/prodotti/prodotto.blade.php
+++ resources/views/prodotti/prodotto.blade.php
@@ -11,6 +11,31 @@
                   <h5>€{{$prodotto->prezzo}}</h5>
                   <p class="card-text">{{$prodotto->descrizione}}</p>
                 </div>
+                @if (Auth::user()->isAdmin == 1)
+
+                
+
+
+
+                        <div class = "form-group">
+                          <form method = "GET" action = "/prodotti/modifica/{{$prodotto->id}}" > 
+                             {{csrf_field()}}
+                            <button type="submit" class="btn btn-primary">Modifica</button>
+                          </form>
+                        </div>
+
+                        
+                        <div class = "form-group">
+                        <form method = "POST" action = "/prodotti/elimina/{{$prodotto->id}}">
+                          {{csrf_field()}}
+                            <button type="submit" class="btn btn-primary">Rimuovi</button>                          
+                          </form>
+                          </div>
+                
+
+                @endif
+
+
                 <div class="card-footer">
                   <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                 </div>
