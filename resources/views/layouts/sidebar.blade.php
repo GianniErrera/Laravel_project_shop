d2a99176740374bf1a15c0fe7685c341f80dd1e0
GianniErrera <giannierrera@yahoo.it>
2018-09-13 21:43:18 +0200

    Aggiunto conteggio numero prodotti per categoria nella barra laterale

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
  <div class="col-lg-3">
 
-          <h1 class="my-4">Emporio da Gianni</h1>
+          <h1 class="my-4">Emporio da zio Gianni</h1>
           <div class="list-group">
             @foreach ($categorie as $categoria)
-            <a href="/categoria/{{$categoria->id}}" class="list-group-item">{{$categoria->nome}}</a>
+            <a href="/categoria/{{$categoria->id}}" class="list-group-item">{{$categoria->nome}} ({{$categoria->prodotto->count()}})</a>
             @endforeach
           </div>
 
